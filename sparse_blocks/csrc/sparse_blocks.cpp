5eacfd690efd3fa84d342d46bd5c6680afb615e4
Varun Agrawal <varagrawal@gmail.com>
2018-07-17 22:22:41 -0400

    python bindings for sparse scatter added

--- sparse_blocks/csrc/sparse_blocks.cpp
+++ sparse_blocks/csrc/sparse_blocks.cpp
@@ -6,6 +6,6 @@ PYBIND11_MODULE(TORCH_EXTENSION_NAME, m)
     // m.def("reduce_mask_backward", &reduce_mask_backward, "Reduce Mask Backward");
     m.def("sparse_gather_forward", &sparse_gather_forward, "Sparse Gather forward");
     m.def("sparse_gather_backward", &sparse_gather_backward, "Sparse Gather backward");
-    // m.def("sparse_scatter_forward", &sparse_scatter_forward, "Sparse Scatter forward");
-    // m.def("sparse_scatter_backward", &sparse_scatter_backward, "Sparse Scatter backward");
+    m.def("sparse_scatter_forward", &sparse_scatter_forward, "Sparse Scatter forward");
+    m.def("sparse_scatter_backward", &sparse_scatter_backward, "Sparse Scatter backward");
 }
--- sparse_blocks/csrc/sparse_blocks.h
+++ sparse_blocks/csrc/sparse_blocks.h
@@ -9,7 +9,7 @@
 #ifdef WITH_CUDA
 #include "cuda/reduce_mask.h"
 #include "cuda/sparse_gather.h"
-// #include "cuda/sparse_scatter.h"
+#include "cuda/sparse_scatter.h"
 #endif
 
 at::Tensor reducemask_forward(const at::Tensor &mask,
@@ -49,7 +49,7 @@ at::Tensor reducemask_forward(const at::Tensor &mask,
     }
 }
 
-at::Tensor sparse_gather_forward(const at::Tensor &x,
+at::Tensor sparse_gather_forward(const at::Tensor &x, // should be NHWC
                                  const at::Tensor &indices,
                                  int blockH, int blockW,
                                  int blockStrH, int blockStrW,
@@ -58,6 +58,8 @@ at::Tensor sparse_gather_forward(const at::Tensor &x,
     if (x.type().is_cuda())
     {
 #ifdef WITH_CUDA
+        // returns tensor of size (NCHW)
+        // the kernel transposes the output
         return sparse_gather_forward_cuda(x, indices,
                                           blockH, blockW,
                                           blockStrH, blockStrW,
@@ -85,5 +87,49 @@ at::Tensor sparse_gather_backward(const at::Tensor &grad_y)
         // else
         // {
         //     return sparse_gather_backward_cpu(grad_y);
+    }
+}
+
+at::Tensor sparse_scatter_forward(const at::Tensor &x, // should be NHWC
+                                  const at::Tensor &indices,
+                                  at::Tensor &ybase,
+                                  int blockH, int blockW,
+                                  int blockStrH, int blockStrW,
+                                  int bOffsH0, int bOffsW0,
+                                  bool add, bool atomic)
+{
+    if (x.type().is_cuda())
+    {
+#ifdef WITH_CUDA
+        // returns tensor of size (NCHW)
+        // the kernel transposes the output
+        return sparse_scatter_forward_cuda(x, indices, ybase,
+                                           blockH, blockW,
+                                           blockStrH, blockStrW,
+                                           bOffsH0, bOffsW0,
+                                           add, atomic);
+#else
+        AT_ERROR("SparseScatter not compiled with GPU support");
+#endif
+        // }
+        // else
+        // {
+        //     return sparse_scatter_forward_cpu(x);
+    }
+}
+
+at::Tensor sparse_scatter_backward(const at::Tensor &grad_y)
+{
+    if (grad_y.type().is_cuda())
+    {
+#ifdef WITH_CUDA
+        return sparse_scatter_backward_cuda(grad_y);
+#else
+        AT_ERROR("SparseScatter not compiled with GPU support");
+#endif
+        // }
+        // else
+        // {
+        //     return sparse_scatter_backward_cpu(grad_y);
     }
 }
