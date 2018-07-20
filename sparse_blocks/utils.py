0dc0bbf9d37db08afe23b7d12bcfe5c276921657
Varun Agrawal <varagrawal@gmail.com>
2018-07-19 20:35:28 -0400

    helper functions for output shape computation

--- sparse_blocks/utils.py
+++ sparse_blocks/utils.py
@@ -36,3 +36,43 @@ def get_padding(height, width, kernel_size=(3, 3), strides=(1, 1), padding=None)
             int(np.ceil(float(pad_w) / 2.0))
 
         return pad_h0, pad_h1, pad_w0, pad_w1
+
+
+def get_out_size_1d(in_size, ksize, stride, padding):
+    """
+    Calculates output size on one dimension.
+
+    :param in_size:  [int]     Input size.
+    :param ksize:    [int]     Kernel size.
+    :param stride:   [int]     Stride size.
+    :param pad:      [string]  Padding method, `SAME` or `VALID`.
+
+    :return          [int]     Output size.
+    """
+
+    if padding == 'VALID':
+        return int(np.ceil(float(in_size - ksize + 1) / float(stride)))
+    elif padding == 'SAME':
+        return int(np.ceil(float(in_size) / float(stride)))
+    else:
+        raise ValueError('Unknown padding method \"{}\"'.format(padding))
+
+
+def get_output_shape(input_shape, out_channels, kernel_size, strides, padding):
+    """Calculates output shape (rank 4) of a 2D convolution operation.
+
+    :param input_shape:  [list]    Input tensor shape.
+    :param out_channels: [list]    Number of channels in output.
+    :param kernel_size:  [list]    Kernel shape.
+    :param strides:      [list]    Strides list.
+    :param padding:      [string]  Padding method, `SAME` or `VALID`.
+
+    :return              [list]    Output tensor shape.
+    """
+    assert len(input_shape) == 4, "input_shape should be of length 4 (NCHW)"
+    return [
+        input_shape[0],
+        out_channels,
+        get_out_size_1d(input_shape[2], kernel_size[0], strides[0], padding),
+        get_out_size_1d(input_shape[3], kernel_size[1], strides[1], padding),
+    ]
